e43e5498a929c994006159f45c9157e51cb2f78f
Seungpyo Suh <me@sspzoa.io>
2025-03-03 00:52:18 +0900

    fix: resolve multiple issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  future: {
-    oxide: false,
-  },
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',

a503fc84e8c827b335d2bfa02e5271acd38be9b2
Seungpyo Suh <me@sspzoa.io>
2025-03-02 17:45:31 +0900

    feat: complete home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,8 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center w-full">
       <Intro information={information} />
       <Projects information={information} projects={projects} />
+      <Contests information={information} />
+      <Future />
     </div>
   );
 }
@@ -32,7 +34,7 @@ interface IntroProps {
 
 const Intro: React.FC<IntroProps> = ({ information }) => {
   return (
-    <div className="flex justify-center items-center w-full min-h-[100dvh] p-9">
+    <div className="flex justify-center items-center w-full min-h-[100dvh]">
       <div className="flex flex-row justify-between items-center w-full max-w-[1200px]">
         <div className="flex flex-col gap-7">
           <div className="flex flex-col gap-4">
@@ -44,26 +46,19 @@ const Intro: React.FC<IntroProps> = ({ information }) => {
             사회적 문제들을 해결하고 <strong>모두가 함께 살 수 있는 세상을 만들기 위해 노력하고 있습니다.</strong>
           </p>
         </div>
-        <Image
-          className="hidden md:block"
-          src="/images/home/luna_model.png"
-          alt="luna_model"
-          width={500}
-          height={418}
-          draggable={false}
-        />
+        <Image src="/images/home/luna_model.png" alt="luna_model" width={500} height={418} draggable={false} />
       </div>
       <DarkScroller />
     </div>
   );
 };
 
-interface ProjectProps {
+interface ProjectsProps {
   information: Information[];
   projects: Project[];
 }
 
-const Projects: React.FC<ProjectProps> = ({ information, projects }) => {
+const Projects: React.FC<ProjectsProps> = ({ information, projects }) => {
   const chunkSize = Math.ceil(projects.length / 3);
   const projectChunks = [
     projects.slice(0, chunkSize),
@@ -131,6 +126,129 @@ const Projects: React.FC<ProjectProps> = ({ information, projects }) => {
           <p className="text-14 font-semibold text-right">루나가 진행한 프로젝트 보러가기</p>
           <Image src="/icons/arrow_forward_ios.svg" alt="arrow_forward_ios" width={16} height={16} />
         </Link>
+      </div>
+    </div>
+  );
+};
+
+interface ContestsProps {
+  information: Information[];
+}
+
+const Contests: React.FC<ContestsProps> = ({ information }) => {
+  const [scrollY, setScrollY] = React.useState(0);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const getTranslateY = () => {
+    const baseSpeed = 0.3;
+    return `translateY(${scrollY * baseSpeed * -1}px)`;
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full py-40">
+      <div className="flex flex-row justify-between items-center w-full max-w-[1200px]">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-3">
+            <p className="text-32 font-medium">
+              <strong className="text-luna-purple">{information[0].contests}개</strong>의 대회,
+              <br />
+              <strong className="text-luna-purple">{information[0].rewards}만원</strong>의 상금 및 지원금
+            </p>
+            <p className="text-wrap text-20 max-w-[500px]">
+              루나는 사회 문제 해결 의지와 능력을 인정받고자 하였고, 그 결과 지금까지{' '}
+              <strong>{information[0].contests}개의 대회에 출전</strong>하여{' '}
+              <strong>상금 및 지원금으로 총 {information[0].rewards}만원</strong>을 받았습니다.
+            </p>
+          </div>
+          <Link href="/awards" className="flex flex-row items-center gap-2 opacity-50 hover:opacity-25 duration-300">
+            <p className="text-14 font-semibold">루나의 업적 알아보기</p>
+            <Image src="/icons/arrow_forward_ios.svg" alt="arrow_forward_ios" width={16} height={16} />
+          </Link>
+        </div>
+        <Image
+          style={{
+            transform: getTranslateY(),
+          }}
+          className="rounded-3xl aspect-[1.5] object-cover translate-y-[500px]"
+          src="/images/home/award.webp"
+          alt="award"
+          width={500}
+          height={418}
+          draggable={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+const Future: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full pt-40 pb-80">
+      <div className="relative isolate">
+        <div className="relative px-20 py-25 bg-[#C7C7C71A] border-2 border-[#fff6] backdrop-blur-sm rounded-[50px] z-20">
+          <p className="text-28 text-center font-medium">
+            루나는 앞으로도 여러분들과 함께
+            <br />
+            <strong>IT기술을 바탕으로 평등한 세상을 만들자는</strong>
+            <br />
+            목표를 향하여 무한히 달려나갈 것입니다
+          </p>
+        </div>
+        <Image
+          className="absolute bottom-[-80px] left-[-70px] z-30 translate-y-[10px]"
+          style={{ animation: 'bounce 4s infinite', animationDelay: '0.2s' }}
+          src="/images/home/future/circle1.png"
+          alt="circle1"
+          width={70}
+          height={70}
+          draggable={false}
+        />
+        <Image
+          className="absolute bottom-[-90px] left-[-40px] z-30 translate-y-[10px]"
+          style={{ animation: 'bounce 4s infinite', animationDelay: '0.1s' }}
+          src="/images/home/future/circle2.png"
+          alt="circle2"
+          width={180}
+          height={180}
+          draggable={false}
+        />
+        <Image
+          className="absolute top-[-70px] right-[-120px] z-10 translate-y-[10px]"
+          style={{ animation: 'bounce 4s infinite', animationDelay: '0.3s' }}
+          src="/images/home/future/circle3.png"
+          alt="circle3"
+          width={280}
+          height={280}
+          draggable={false}
+        />
+        <Image
+          className="absolute top-[-100px] left-[-50px] z-10 translate-y-[10px]"
+          style={{ animation: 'bounce 4s infinite', animationDelay: '0.4s' }}
+          src="/images/home/future/luna.png"
+          alt="luna"
+          width={160}
+          height={160}
+          draggable={false}
+        />
+        <Image
+          className="absolute bottom-[-240px] right-[-240px] z-10 translate-y-[10px]"
+          style={{ animation: 'bounce 4s infinite', animationDelay: '0s' }}
+          src="/images/home/future/hand.png"
+          alt="hand"
+          width={430}
+          height={430}
+          draggable={false}
+        />
       </div>
     </div>
   );

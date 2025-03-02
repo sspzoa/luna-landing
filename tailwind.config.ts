45292a694a67bcf24407fd38e07ea6eb241da492
Seungpyo Suh <me@sspzoa.io>
2025-03-03 00:50:19 +0900

    fix3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  future: {
+    oxide: false,
+  },
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',

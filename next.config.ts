aa34a23fd6a1e34ee1f3e0c76328993525268445
Seungpyo Suh <me@sspzoa.io>
2025-03-08 19:52:42 +0900

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        hostname: '**',
       },
     ],
   },

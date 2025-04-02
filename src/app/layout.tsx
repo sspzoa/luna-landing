40f9177452b9baca157e109921ced167ced7419b
Seungpyo Suh <me@sspzoa.io>
2025-04-02 11:09:23 +0900

    feat: add seo optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,8 @@ import type React from 'react';
 export const metadata: Metadata = {
   title: 'IT 소셜벤처 동아리, LUNA',
   description: '한국디지털미디어고등학교의 IT 소셜벤처 동아리.',
-  keywords: '루나,LUNA,디미고,DIMIGO,동아리,IT소셜벤처',
+  keywords:
+    '루나,LUNA,디미고,DIMIGO,동아리,IT소셜벤처,한국디지털미디어고등학교,디미고 동아리,디미고 입학,디미고 현실,디미고 수준,디미고 논란,디미고 경쟁률,디미고 홈페이지,디미고 내신 커트라인,디미고 현실 디시,디미고 학비,디미고 입결,디미고 급식,디미고 갤러리,디미고 교복,디미고갤,디미고 기숙사,디미고 커트라인,디미고 내신',
   openGraph: {
     images: [{ url: 'https://luna.codes/images/og-image.png' }],
   },
